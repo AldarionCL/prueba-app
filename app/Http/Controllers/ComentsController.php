80410d70f0550b97975f078d29c72fa55669f6c8
Aldarion <aldarion.neologik@gmail.com>
2023-05-03 20:01:53 -0400

    Commit inicial migraciones

---
+++ app/Http/Controllers/ComentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\coments;
+use Illuminate\Http\Request;
+
+class ComentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(coments $coments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(coments $coments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, coments $coments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\coments  $coments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(coments $coments)
+    {
+        //
+    }
+}
